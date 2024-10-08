3db9a6878b780a9db6d418db55a72a748928be33
xiaolou86 <20718693+xiaolou86@users.noreply.github.com>
2024-10-08 14:15:56 +0800

    add app.py

---
+++ app.py
@@ -0,0 +1,25 @@
+from flask import Flask
+from models import db
+import os
+from routes import init_app
+
+app = Flask(__name__)
+app.config.from_object('config.Config')
+
+# 初始化数据库
+db.init_app(app)
+
+# 初始化 API 路由
+init_app(app)
+
+@app.route('/')
+def home():
+    return "Welcome to the Management Platform"
+
+# 运行应用
+if __name__ == "__main__":
+    with app.app_context():
+        if not os.path.exists('database.db'):
+            db.create_all()  # 创建数据库表
+    app.run(debug=True)
+
